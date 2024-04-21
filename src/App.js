e237fa4b597b8f69dfe35c9b4485ea490dab742c
shayan-malik <Shayan.ulhaq@ceative.co.uk>
2024-04-21 21:58:36 +0500

    newchanges

--- src/App.js
+++ src/App.js
@@ -1,48 +1,3 @@
-// import React, { useState } from 'react';
-// import AddEmployeeForm from './component/AddEmployeeForm';
-// import ViewHierarchy from './component/ViewHierarchy';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// const App = () => {
-//   const [employees, setEmployees] = useState([]);
-
-//   const handleAddEmployee = (data) => {
-//     setEmployees([...employees, { id: employees.length + 1, ...data }]);
-//   };
-
-//   const handleDeleteRecord = (id) => {
-//     toast.success('Empoyee deleted successfully ');
-//     setEmployees(employees.filter((obj) => obj.id !== id));
-//   }
-//   const preEmployees = [...employees];
-//   const handleSearch = (val) => {
-//     if (val.length) {
-//       const filtered = employees.filter((obj) => obj.employee.includes(val) || obj.supervisor.includes(val));
-//       setEmployees(filtered)
-//     }
-//     else {
-//       setEmployees(preEmployees)
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <h1 style={{ textAlign: 'center' }}>Employee Management System</h1>
-//       <AddEmployeeForm onSubmit={handleAddEmployee} />
-//       <ViewHierarchy
-//         hierarchy={employees}
-//         handleDelete={handleDeleteRecord}
-//         handleSearch={handleSearch}
-//       />
-//       <ToastContainer />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import React, { useState } from 'react';
 import AddEmployeeForm from './component/AddEmployeeForm';
 import ViewHierarchy from './component/ViewHierarchy';
@@ -53,16 +8,17 @@ const App = () => {
   const [employees, setEmployees] = useState([]);
   const [originalEmployees, setOriginalEmployees] = useState([]);
 
+
   const handleAddEmployee = (data) => {
-    const newEmployee = { id: employees.length + 1, ...data };
+    const newEmployee = { id: employees?.length + 1, ...data };
     setEmployees([...employees, newEmployee]);
     setOriginalEmployees([...originalEmployees, newEmployee]);
   };
 
   const handleDeleteRecord = (id) => {
     toast.success('Employee deleted successfully ');
-    setEmployees(employees.filter((obj) => obj.id !== id));
-    setOriginalEmployees(originalEmployees.filter((obj) => obj.id !== id));
+    setEmployees(employees.filter((obj) => obj?.id !== id));
+    setOriginalEmployees(originalEmployees.filter((obj) => obj?.id !== id));
   };
 
   const handleSearch = (val) => {
@@ -85,7 +41,7 @@ const App = () => {
         handleDelete={handleDeleteRecord}
         handleSearch={handleSearch}
       />
-      <ToastContainer autoClose={1000}/>
+      <ToastContainer autoClose={1000} />
     </div>
   );
 };
--- src/component/ViewHierarchy.js
+++ src/component/ViewHierarchy.js
@@ -5,36 +5,42 @@ const ViewHierarchy = ({ hierarchy, handleDelete, handleSearch }) => {
   return (
     <div className='hierarchy_wrapper'>
       <h2>Employee Hierarchy</h2>
-      <input
-        name='search'
-        className='search_bar'
-        placeholder='Search here...'
-        onChange={({ target }) => handleSearch(target.value)}
-      />
-      <table>
-        <thead>
-          <tr>
-            <th>Sr no#</th>
-            <th>Employee Name</th>
-            <th>Supervisor</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {hierarchy.length ? hierarchy.map((obj) => (
-            <tr key={obj.id}>
-              <td>{obj.id}</td>
-              <td>{obj.employee}</td>
-              <td>{obj.supervisor}</td>
-              <td>
-                <button className='add_btn' onClick={() => handleDelete(obj.id)}>Delete</button>
-              </td>
+      <div>
+        <input
+          name='search'
+          className='search_bar'
+          placeholder='Search here...'
+          onChange={({ target }) => handleSearch(target.value)}
+        />
+      </div>
+
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Sr no#</th>
+              <th>Employee Name</th>
+              <th>Supervisor</th>
+              <th>Actions</th>
             </tr>
-          )) :
-            <p>No records found!</p>}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {hierarchy.length ? hierarchy.map((obj) => (
+              <tr key={obj.id}>
+                <td>{obj.id}</td>
+                <td>{obj.employee}</td>
+                <td>{obj.supervisor}</td>
+                <td>
+                  <button className='add_btn' onClick={() => handleDelete(obj.id)}>Delete</button>
+                </td>
+              </tr>
+            )) :
+              <p style={{ textAlign: 'center' }}>No records found!</p>}
+          </tbody>
+        </table>
+      </div>
     </div>
+
   );
 };
 
