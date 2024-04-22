7cf5786e786338676f96b66c67388e668663d1e9
shayan-malik <Shayan.ulhaq@ceative.co.uk>
2024-04-22 12:21:14 +0500

    newchanges

--- src/component/ViewHierarchy.js
+++ src/component/ViewHierarchy.js
@@ -1,53 +1,3 @@
-// import React from 'react';
-// import './style.css';
-
-// const ViewHierarchy = ({ hierarchy, handleDelete, handleSearch }) => {
-//   return (
-//     <div className='hierarchy_wrapper'>
-//       <h2>Employee Hierarchy</h2>
-//       <div>
-//         <input
-//           name='search'
-//           className='search_bar'
-//           placeholder='Search here...'
-//           onChange={({ target }) => handleSearch(target.value)}
-//         />
-//       </div>
-
-//       <div class="table-container">
-//         <table>
-//           <thead>
-//             <tr>
-//               <th>Sr no#</th>
-//               <th>Employee Name</th>
-//               <th>Supervisor</th>
-//               <th>Actions</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {hierarchy.length ? hierarchy.map((obj) => (
-//               <tr key={obj.id}>
-//                 <td>{obj.id}</td>
-//                 <td>{obj.employee}</td>
-//                 <td>{obj.supervisor}</td>
-//                 <td>
-//                   <button className='add_btn' onClick={() => handleDelete(obj.id)}>Delete</button>
-//                 </td>
-//               </tr>
-//             )) :
-//               <p style={{ textAlign: 'center' }}>No records found!</p>}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-
-//   );
-// };
-
-// export default ViewHierarchy;
-
-
-
 import React, { useState } from 'react';
 import './style.css';
 
@@ -82,7 +32,7 @@ const ViewHierarchy = ({ hierarchy, handleDelete, handleSearch }) => {
     if (sortConfig.key === columnKey) {
       return sortConfig.direction === 'ascending' ? ' 🔼' : ' 🔽';
     }
-    return null; // or return a default icon if you prefer every column to have an icon always
+    return null;
   };
 
   return (
